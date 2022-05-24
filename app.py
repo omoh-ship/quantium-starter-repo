f97d022abd038453cff4e8ec2c9a8b09d240be7f
Gloria <isedugloria@gmail.com>
2022-05-24 06:48:56 +0100

    color pallete tuning

--- app.py
+++ app.py
@@ -7,21 +7,33 @@ import pandas as pd
 app = Dash(__name__)
 
 
-colors = {
-    'background': "#D9FAFF",
-    'text': "#00204A",
+COLORS = {
+    'background': "#171717",
+    'miscellaneous': "#444444",
+    'text': "#EDEDED",
 }
+
+
+def create_visualisation(data_frame):
+    line_plot = line(data_frame, x='Date', y='Sales($)', color='Region')
+    line_plot.update_layout(
+        plot_bgcolor=COLORS['background'],
+        paper_bgcolor=COLORS['miscellaneous'],
+        font_color=COLORS['text']
+    )
+    return line_plot
+
 
 header = html.H1(children='Pink Morsel Sales Visualization', style={
                 'textAlign': 'center',
-                'color': colors['text']
+                'color': COLORS['text']
             })
 description = html.Div(children='A visualization of the sales of the pink morsel', style={
                     'textAlign': 'center',
-                    'color': colors['text']
+                    'color': COLORS['text']
                 })
 radio_options = dcc.RadioItems(id='region', options=['north', 'south', 'east', 'west', 'all'], value='all',
-                               style={'color': colors['text']})
+                               style={'color': COLORS['text']})
 date_range = dcc.DatePickerRange(
             id='my-date-picker',
             min_date_allowed=date(2018, 2, 6),
@@ -31,7 +43,7 @@ date_range = dcc.DatePickerRange(
             display_format='YYYY-MM-DD')
 graph = dcc.Graph(id='graph', figure={})
 
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+app.layout = html.Div(style={'backgroundColor': COLORS['background']}, children=[
     header,
     description,
     radio_options,
@@ -52,7 +64,7 @@ def update_output(region, start_date, end_date):
     else:
         area_df = df
     date_query = area_df[area_df['Date'].between(start_date, end_date)]
-    return line(date_query, x='Date', y='Sales($)', color='Region')
+    return create_visualisation(date_query)
 
 
 if __name__ == '__main__':
